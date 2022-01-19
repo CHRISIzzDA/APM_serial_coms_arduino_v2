de6ce2c71b6f390d43e5694a49ee3743bea829ef
CHRISIzzDA <77968773+CHRISIzzDA@users.noreply.github.com>
2022-01-19 14:48:47 +0100

    Added Test Programm
    Added Automated response to test Raspberry Pi Programm

--- src/main.cpp
+++ src/main.cpp
@@ -10,20 +10,72 @@
 #define fan1 9
 #define fan2 6
 
-char *token;
-const char *delimiter = ";"; //old <>;
-char  myString[64];
-char pumpData[4];   //old 6
-int  iPumpData = 0;
-char fanData[4];    //old 6
-int iFanData = 0;
+typedef struct
+{
+    uint16_t _depth;
+    uint16_t _flow;
+}Pair_16_t;
+//----------------------------------------------------------------------------------------------------------------------
+
+
+// Variables
 const byte numChars = 64;
 char receivedChars[numChars]; // an array to store the received data
+char  myString[numChars];
+char *token;
+const char *delimiter = ";";
+
+char pumpData[4];
+char* endPtrPump;
+int  iPumpData = 0;
+
+char fanData[4];
+char* endPtrFan;
+int iFanData = 0;
+
+//all Depth values are calculated by depth in m / 0.05859375
+
+Pair_16_t  dataLvl1[5] = {{512,0},
+                          {529,0},
+                          {495,0},
+                          {563,0},
+                          {546,0}};
+int dataCounterLvl1 = 0;
+
+Pair_16_t dataLvl2[5] = {{495, 15},
+                         {461, 15},
+                         {427, 15},
+                         {444, 15},
+                         {427, 15}};
+int dataCounterLvl2 = 0;
+
+Pair_16_t dataLvl3[5] = {{393, 20},
+                         {375, 20},
+                         {341, 20},
+                         {324, 20},
+                         {341, 20}};
+int dataCounterLvl3 = 0;
+
+Pair_16_t dataLvl4[5] = {{307, 25},
+                         {273, 25},
+                         {239, 25},
+                         {221, 25},
+                         {256, 25}};
+int dataCounterLvl4 = 0;
+
+Pair_16_t dataLvl5[5] = {{324, 8},
+                        {393, 3},
+                        {444, 0},
+                        {495, 0},
+                        {512, 0}};
+int dataCounterLvl5 = 0;
 
 boolean newData = false;
-boolean inputRecieved = false;
+boolean inputReceived = false;
+//----------------------------------------------------------------------------------------------------------------------
 
 
+//Functions
 void recvWithEndMarker() {
     static byte ndx = 0;
     char endMarker = '\n';
@@ -48,6 +100,7 @@ void recvWithEndMarker() {
     }
 }
 
+//------------------------------------------------------------------------
 void setNewData() {
     if (newData) {
         strcpy(myString, receivedChars);
@@ -58,21 +111,18 @@ void setNewData() {
             switch (i) {
                 case 0:
                     strcpy(pumpData, token);
-                    
                     //converting into int
-                    iPumpData = atoi(pumpData);
+                    iPumpData = strtol(pumpData, &endPtrPump, 10);
+
                     analogWrite(pump, iPumpData);
-                    Serial.print(iPumpData);
                     break;
                 case 1:
                     strcpy(fanData, token);
-                     
-                    Serial.print(";");
-
                     //converting into int
-                    iFanData = atoi(fanData);
-                    
-                    Serial.println(iFanData);
+                    iFanData = strtol(fanData, &endPtrFan, 10);
+
+                    analogWrite(fan1, iFanData);
+                    analogWrite(fan2, iFanData);
                     break;
                 default:
                     Serial.println(token);
@@ -82,18 +132,67 @@ void setNewData() {
             token = strtok(nullptr, delimiter);
         }
         newData = false;
-        inputRecieved = true;
+        inputReceived = true;
     }
 }
 
+//----------------------------------------------------------------------
 void sendData() {
-    if (inputRecieved) {
-        //sending out Response
-        
-       
-        inputRecieved = false;
+    if (inputReceived) {
+
+        switch (iPumpData) {
+            case 0:
+                if (iFanData == 0) {
+                    Serial.print(dataLvl1[dataCounterLvl1]._depth);
+                    Serial.print(";");
+                    Serial.println(dataLvl1[dataCounterLvl1]._flow);
+                    if (dataCounterLvl1 < (sizeof(dataLvl1) / sizeof(Pair_16_t) )- 1) {
+                        ++dataCounterLvl1;
+                    }
+                }else {
+                    Serial.print(dataLvl5[dataCounterLvl5]._depth);
+                    Serial.print(";");
+                    Serial.println(dataLvl5[dataCounterLvl5]._flow);
+                    if (dataCounterLvl5 < (sizeof(dataLvl5) / sizeof(Pair_16_t)) - 1) {
+                        ++dataCounterLvl5;
+                    }
+                }
+                break;
+            case 100:
+                Serial.print(dataLvl2[dataCounterLvl2]._depth);
+                Serial.print(";");
+                Serial.println(dataLvl2[dataCounterLvl2]._flow);
+                if (dataCounterLvl2 < (sizeof(dataLvl2) / sizeof(Pair_16_t) )- 1) {
+                    ++dataCounterLvl2;
+                }
+                break;
+            case 500:
+                Serial.print(dataLvl3[dataCounterLvl3]._depth);
+                Serial.print(";");
+                Serial.println(dataLvl3[dataCounterLvl3]._flow);
+                if (dataCounterLvl3 < (sizeof(dataLvl3) / sizeof(Pair_16_t) )- 1) {
+                    ++dataCounterLvl3;
+                }
+                break;
+            case 1000:
+                Serial.print(dataLvl4[dataCounterLvl4]._depth);
+                Serial.print(";");
+                Serial.println(dataLvl4[dataCounterLvl4]._flow);
+                if (dataCounterLvl4 < (sizeof(dataLvl4) / sizeof(Pair_16_t) )- 1) {
+                    ++dataCounterLvl4;
+                }
+                break;
+            default:
+                Serial.print(iPumpData);
+                Serial.print(";");
+                Serial.println(iFanData);
+                break;
+        }
+
+        inputReceived = false;
     }
 }
+//----------------------------------------------------------------------------------------------------------------------
 
 void setup() {
     pinMode(statusLED, OUTPUT);
@@ -115,15 +214,4 @@ void loop(){
     setNewData();
     sendData();
 
-    //obsolete
-/*
-    if (receive_buffer == "on")
-    {
-        digitalWrite(statusLED , HIGH);
-    }
-    else if (receive_buffer == "off")
-    {
-        digitalWrite(statusLED, LOW);
-    }
-*/
 }
